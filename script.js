6e3f0e158bdec7a34638bae06c879f4ba0205a23
Joseph Davitt <joseph.j.davitt@gmail.com>
2025-12-03 17:43:58 -0800

    add about me

--- script.js
+++ script.js
@@ -3,9 +3,6 @@
 document.body.addEventListener('click', () => {
     document.body.style.backgroundColor = getRandomColor();
     document.getElementById('project-container').style.backgroundColor = getRandomColor();
-    document.getElementsByClassName('btn btn-primary').forEach(btn => {
-        btn.style.backgroundColor = getRandomColor();
-    });
 });
 
 document.body.style.transition = 'background-color 0.5s ease';
@@ -29,6 +26,7 @@ function clearPage() {
 
 // grab random element on page and remove after timeout
 function clearPageRandomly() {
+    alert("You were warned!");
     const elements = Array.from(document.body.children);
     let delay = 2000;
     while (elements.length > 0) {
